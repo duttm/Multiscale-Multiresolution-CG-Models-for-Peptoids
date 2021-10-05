3d3cfc6ec60871bcf61efba941c298af0e495367
polo <akashneon@gmail.com>
2021-10-05 11:51:53 -0400

    mods

--- P3/Fix_FF_Errors/model.bash
+++ P3/Fix_FF_Errors/model.bash
@@ -2,17 +2,6 @@
 
 ## Last exec: ./model.bash (on 4th October 2021)
 
-## Note: initial pdb file needs to made with an unmodified charmm force field. 
-## Use: 
-## 1. ./22to36converter.bash (Check if all CG2O1 atoms are converted to TC)
-## 2.  python cgenff_charmm2gmx_py3_nx2.py SRUG SRUG.mol2 SRUG_copy.str charmm36-nov2016.ff/ (last execuation on 4th October 2021)
-## 3. Use the charmm-modifed folder to run an energy minimization simulation
-## 4. Make changes to the topology file: point to the modified charmm FF folder. 
-## 5. You will get errors, fix them one by one. 
-##################################################################################################################
-
-## If you run the above commands, it will generate a new .top and .prm file. These files need to be modified to accomodate the hybrid C22 and C36 parameters. The modfied version is kept in a subfolder (XXXXXX) for safe keeping.
-
 
 input=srug_ini
 
